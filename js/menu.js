eb1cc392b0c6cdbcea0a43078bbd49e308d24246
AlonaMaliarchyk <alenasymorot@gmail.com>
2022-08-29 15:57:54 +0200

    start tablet menu

--- js/menu.js
+++ js/menu.js
@@ -3,21 +3,18 @@
     openMenuBtn: document.querySelector('.menu-open'),
     closeMenuBtn: document.querySelector('.menu-close'),
     menu: document.querySelector('.mob-menu'),
-    body: document.querySelector('body'),
-    menuList: document.querySelector('.mob-menu-list'),
+    body: document.querySelector('body')
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
-  refs.closeMenuBtn.addEventListener('click', toggleMenu);
-  refs.menuList.addEventListener('click', removeMenu);
+  refs.closeMenuBtn.addEventListener('click', closeMenu);
 
   function toggleMenu() {
-    refs.menu.classList.toggle('is-hidden');
-    refs.body.classList.toggle('no-scroll');
+    refs.menu.classList.remove('is-hidden');
     refs.body.classList.toggle('no-scroll');
   }
 
-  function removeMenu() {
+  function closeMenu() {
     refs.menu.classList.add('is-hidden');
   }
 })();
